65b659b618cd33e35d5f1363d02531ce41a017d9
evaaldo <evaaldormj@gmail.com>
2022-11-22 03:48:15 -0300

    Criação da lógica que muda as cores conforme aumenta a dificuldade

--- js/forca.js
+++ js/forca.js
@@ -4,15 +4,19 @@ const chkN = document.getElementById('generator__number')
 const chkS = document.getElementById('generator__symbol')
 
 const forca = document.querySelector('.medidor__texto')
-const barra = document.querySelector('.barra')
+const b1 = document.getElementById('barra-1')
+const b2 = document.getElementById('barra-2')
+const b3 = document.getElementById('barra-3')
+const b4 = document.getElementById('barra-4')
 
-const lista = [chkU, chkL, chkN, chkS]
+const listaBarras = [b1, b2, b3, b4]
+const listaChk = [chkU, chkL, chkN, chkS]
 let count = 0
 let valorFinal
 
 function verificaChecked() {
 
-    lista.forEach((el) => {
+    listaChk.forEach((el) => {
         if(el.checked == true) {
             count++
         }
@@ -20,10 +24,34 @@ function verificaChecked() {
         let valorFinal = count
         
         alteraForca(valorFinal)
+        alteraCor(valorFinal)
 
     })
     
     count = 0
+
+}
+
+function alteraCor(valorFinal) {
+
+    listaBarras.forEach((el) => {
+
+        if(valorFinal == 1) {
+            el.style.background = 'var(--verde-escuro)'
+        }
+        if(valorFinal == 2) {
+            el.style.background = 'var(--laranja)'
+        }
+        if(valorFinal == 3) {
+            el.style.background = 'var(--vermelho)'
+        }
+        if(valorFinal == 4) {
+            el.style.background = 'var(--vermelho-muito-escuro)'
+        }
+        if(valorFinal == 0) {
+            el.style.background = 'var(--cinza)'
+        }
+    })
 
 }
 
